bbf2d82c2eb703f4decee97a2d01ae3aac1e62c1
Arthur Freitas <12202444@aluno.cotemig.com.br>
2024-08-19 18:00:01 -0300

    atualizacoes

--- View/html/contato.php
+++ View/html/contato.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Contato</title>
-    <link rel="stylesheet" href="../css/contatos.css">
+    <link rel="stylesheet" href="../css/contato.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="nav.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+          <h3>FindEditor</h3>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="nav.php" class="nav-link px-2 link-body-emphasis">Home</a></li>
+          <li><a href="contato.php" class="nav-link px-2 link-secondary">Contato</a></li>
+          <li><a href="sobre.html" class="nav-link px-2 link-body-emphasis">Sobre</a></li>
+          <li><a href="faq.html" class="nav-link px-2 link-body-emphasis">FAQ</a></li>
+        </ul>
+
+
+        <div class="dropdown text-end">
+          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small">
+            <li><a class="dropdown-item" href="perfil.php">Perfil</a></li>
+            <li><a class="dropdown-item" href="#">Criar Serviço</a></li>
+            <li><a class="dropdown-item" href="#">Meus Serviços</a></li>
+            <li><a class="dropdown-item" href="#">Favoritos</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Log out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
     <div class="container">
         <a href="nav.php" class="back-button">&larr; Voltar</a>
         <h1>Contato</h1>
@@ -16,5 +51,11 @@
             <input type="submit" value="Enviar">
         </form>
     </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
